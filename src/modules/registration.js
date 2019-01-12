2f0946053946a30a777626d77dc40dbaeff59e3b
jitendra <jitendra.thakur@impetus.co.in>
2019-01-12 12:00:19 +0530

    added aggrid and routing

--- src/modules/registration.js
+++ src/modules/registration.js
@@ -5,19 +5,24 @@
 		el : '#CustomerForm',
 
 		initialize : function() {
-			this.customerListView = new CustomerListView();
+			
+			//this.customerListView = new CustomerListView();
 		},
 
 		events : {
 			'click button' : 'addCustomerCollection'
 		},
 
-		addCustomerCollection : function(e) {					
+		addCustomerCollection : function(e) {	
+			window.location.hash = '/about';	
+
 			var customer = new Customer({
 				email : this.$('input[name=email]').val(),				
 				mobile : this.$('input[name=mobile]').val(),
 			});
-			this.customerListView.collection.add(customer);			
+			//this.customerListView.collection.add(customer);		
+
+			
 		}
 	});
 
@@ -26,34 +31,7 @@
 			this.email = 'email';
 			this.mobile = 'mobile';
 		}
-	});
-
-	var columnDefs = [
-      {headerName: "Email", field: "email"},
-      {headerName: "Mobile", field: "mobile"}      
-    ];
-
-    // let the grid know which columns and what data to use
-    var gridOptions = {
-      columnDefs: columnDefs,
-      enableSorting: true,
-      enableFilter: true
-    };
-
-    var eGridDiv = document.querySelector('#myGrid');
-	new agGrid.Grid(eGridDiv, gridOptions); 
-
-	var data = data || [];
-	var ListView = Backbone.View.extend({		
-		render : function() {
-						
-			var payload = {"email":this.model.get('email'),"mobile":this.model.get('mobile')};
-			data.push(payload);
-									
-			gridOptions.api.setRowData(data);
-			return this;
-		}
-	});
+	});	
 
 	var CustomerList = Backbone.Collection.extend({
 		model : Customer
@@ -85,28 +63,78 @@
 		}
 	});
 
-	new UserInputView();
+	
+
+	var homeView = '<form id="CustomerForm">';
+        homeView += '<div class="container">';
+            homeView += '<h1>Customer Information</h1>';
+            homeView += '<p>Please put customer information</p>';
+            homeView += '<hr>';
+            homeView += '<label for="email"><b>Email</b></label>';
+            homeView += '<input type="text" placeholder="Enter Email" name="email">';
+            homeView += '<label for="mobile"><b>Mobile</b></label>';
+            homeView += '<input type="text" placeholder="Mobile" name="mobile">';
+            homeView += '<button type="button" class="registerbtn">Register</button>';
+        homeView += '</div>';
+        homeView += '<div class="container signin">';
+            homeView += '<p>Already have an account? <a href="#">Sign in</a>.</p>';
+        homeView += '</div>';
+    homeView += '</form>';
 
 	var HomeView = Backbone.View.extend({
-	      template: '<h1>Home</h1>',
-	      initialize: function () {
-	          this.render();
-	      },
-	      render: function () {
-	          this.$el.html(this.template);
-	      }
+	    template: homeView,
+	    initialize: function () {
+	        this.render();
+	    },
+	    render: function () {
+	        this.$el.html(this.template);
+	        setTimeout(function() {
+	        	new UserInputView();
+	        },3000)
+	        
+	    }
 	});
 	  
 
 	var AboutView = Backbone.View.extend({
-	      template: '<h1>About</h1>',
-	      initialize: function () {
-	          this.render();
-	      },
-	      render: function () {
-	          this.$el.html(this.template);
-	      }
-	  });
+	    template: '<div id="myGrid" style="height: 200px;width:800px;" class="ag-theme-balham"></div>',
+	    initialize: function () {
+	        this.render();
+	    },
+	    render: function () {
+	        this.$el.html(this.template);
+	        setTimeout(function() {
+	        	var columnDefs = [
+			      {headerName: "Email", field: "email"},
+			      {headerName: "Mobile", field: "mobile"}      
+			    ];
+
+			    // let the grid know which columns and what data to use
+			    var gridOptions = {
+			      columnDefs: columnDefs,
+			      enableSorting: true,
+			      enableFilter: true
+			    };
+
+			    var eGridDiv = document.querySelector('#myGrid');
+				new agGrid.Grid(eGridDiv, gridOptions); 
+
+				var data = data || [];
+				var payload = {"email":"test@test.com","mobile":"3434344343"};
+						data.push(payload);
+												
+						gridOptions.api.setRowData(data);
+				var ListView = Backbone.View.extend({		
+					render : function() {
+									
+						//var payload = {"email":this.model.get('email'),"mobile":this.model.get('mobile')};
+						
+						return this;
+					}
+				});
+	        }, 1000)
+	    }
+	});
 	  
 	  var AppRouter = Backbone.Router.extend({
 	      routes: {          
@@ -126,7 +154,5 @@
 
 	  var appRouter = new AppRouter();
 	  Backbone.history.start();
-
-   
 
 })(jQuery);
